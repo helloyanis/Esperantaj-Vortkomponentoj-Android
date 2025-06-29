4d6779e985ea598f39f2fd9e4614290bccc78e9a
🦊 helloyanis <helloyanis@hotmail.com>
2025-06-29 15:06:45 +0200

    Yeah let's go!

---
+++ app/src/main/java/dev/helloyanis/vortkom/esperanto/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.helloyanis.vortkom.esperanto;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
